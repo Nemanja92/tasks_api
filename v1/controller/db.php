a95a454de4a464eb21026d89c3979baf01a4ece8
Nemanja92 <nemanja92@icloud.com>
2020-02-23 00:35:13 -0500

    Database class default password fix

--- v1/controller/db.php
+++ v1/controller/db.php
@@ -7,20 +7,20 @@ class DB {
 
   public static function connectWriteDB() {
     if(self::$writeDBConnection === null) {
-      self::$writeDBConnection = new PDO('mysql:host=localhost;dbname=tasksds;charset=utf8', 'root', 'root');
+      self::$writeDBConnection = new PDO('mysql:host=localhost;dbname=tasksdb;charset=utf8', 'root', '');
       self::$writeDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       self::$writeDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }
-    return self::$writeDBConnection
+    return self::$writeDBConnection;
   }
 
   public static function connectReadDB() {
     if(self::$readDBConnection === null) {
-      self::$readDBConnection = new PDO('mysql:host=localhost;dbname=tasksds;charset=utf8', 'root', 'root');
+      self::$readDBConnection = new PDO('mysql:host=localhost;dbname=tasksdb;charset=utf8', 'root', '');
       self::$readDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       self::$readDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }
-    return self::$readDBConnection
+    return self::$readDBConnection;
   }
 
 
